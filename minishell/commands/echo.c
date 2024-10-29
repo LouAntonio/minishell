9114c5ebd4bbda54a4ca778e043f9278727b6a34
Lourenco Antonio Dala <lantonio@c1r2s6.42luanda.com>
2024-10-29 08:35:13 +0100

    echo and pwd with output redirections

--- minishell/commands/echo.c
+++ minishell/commands/echo.c
@@ -6,21 +6,43 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:52:12 by lantonio          #+#    #+#             */
-/*   Updated: 2024/10/17 16:27:10 by lantonio         ###   ########.fr       */
+/*   Updated: 2024/10/29 08:34:18 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	echo(char *str, int option)
+void	echo(char **str)
 {
 	int	i;
+	int	fd;
+	int	vet_len;
 
-	i = 4;
-	if (!option)
-		i = 7;
-	printf("%s", str+(i + 1));
-	if (!option)
-		printf("\033[30m\033[47m%%\033[0m");
-	printf("\n");
+	i = 0;
+	fd = 1;
+	vet_len = 0;
+	while (str[vet_len])
+		vet_len++;
+	if (vet_len >= 2)
+	{
+		if (!ft_strcmp(str[vet_len - 2], ">"))
+			fd = open(str[vet_len - 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		else if (!ft_strcmp(str[vet_len - 2], ">>"))
+			fd = open(str[vet_len - 1], O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+		if (!ft_strcmp(str[1], "-n"))
+			i = 1;
+	}
+	while (str[++i])
+	{
+		if (!(!ft_strcmp(str[i], ">") || !ft_strcmp(str[i], ">>") || !ft_strcmp(str[i - 1], ">") || !ft_strcmp(str[i - 1], ">>")))
+				ft_putstr_fd(str[i], fd);
+		if (str[i + 1])
+			ft_putstr_fd(" ", fd);
+	}
+	if (str[1])
+		if (!ft_strcmp(str[1], "-n"))
+			ft_putstr_fd("\033[30m\033[47m%\033[0m", fd);
+	ft_putstr_fd("\n", fd);
+	if (fd != 1)
+		close(fd);
 }
--- minishell/commands/pwd.c
+++ minishell/commands/pwd.c
@@ -6,19 +6,39 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:10:13 by lantonio          #+#    #+#             */
-/*   Updated: 2024/10/16 15:42:19 by lantonio         ###   ########.fr       */
+/*   Updated: 2024/10/29 08:16:46 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	pwd(void)
+void	pwd(char **str)
 {
+	int		fd;
+	int		vet_len;
 	char	path[PATH_MAX];
 
+	fd = 1;
+	vet_len = 0;
+	while (str[vet_len])
+		vet_len++;
+	if (vet_len > 2)
+	{
+		if (!ft_strcmp(str[vet_len - 2], ">"))
+			fd = open(str[vet_len - 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		else if (!ft_strcmp(str[vet_len - 2], ">>"))
+			fd = open(str[vet_len - 1], O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	}
+	if (vet_len > 3)
+	{
+		printf("pwd: too many arguments\n");
+		return ;
+	}
 	if (getcwd(path, sizeof(path)) != NULL)
-		printf("%s", path);
+		ft_putstr_fd(path, fd);
 	else
 		perror("Erro ao obter o diretório");
-	printf("\n");
+	ft_putstr_fd("\n", fd);
+	if (fd != 1)
+		close(fd);
 }
