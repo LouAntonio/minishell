7232175f2c6e6d6de23c7a602d480c9b438bfc56
Lourenco Antonio Dala <lantonio@c1r2s6.42luanda.com>
2024-10-31 08:35:09 +0100

    codes integration

--- minishell/commands/pwd.c
+++ minishell/commands/pwd.c
@@ -6,42 +6,55 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:10:13 by lantonio          #+#    #+#             */
-/*   Updated: 2024/10/29 13:02:02 by lantonio         ###   ########.fr       */
+/*   Updated: 2024/10/31 08:13:19 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	pwd(char **str)
+void	write_path_on_fd(int fd)
 {
-	int		fd;
-	int		vet_len;
 	char	path[PATH_MAX];
 
+	if (getcwd(path, sizeof(path)) != NULL)
+		ft_putstr_fd(path, fd);
+	else
+		perror("Erro ao obter o diretório");
+}
+
+void	pwd(char **str)
+{
+	int		i;
+	int		fd;
+	int		vet_len;
+
+	i = 0;
 	fd = 1;
 	vet_len = 0;
 	while (str[vet_len])
 		vet_len++;
-	if (vet_len > 2)
+	if (vet_len > 1)
 	{
-		if (!ft_strcmp(str[vet_len - 2], ">"))
-			fd = open(str[vet_len - 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-		else if (!ft_strcmp(str[vet_len - 2], ">>"))
-			fd = open(str[vet_len - 1], O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+		while (str[++i])
+		{
+			if (ft_strcmp(str[1], ">") != 0 && ft_strcmp(str[1], ">>") != 0 && ft_strcmp(str[1], "<") != 0)
+			{
+				printf("pwd: too many arguments\n");
+				break ;
+			}
+			if (!ft_strcmp(str[i], ">"))
+				fd = open(str[i + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+			else if (!ft_strcmp(str[i], ">>"))
+				fd = open(str[i + 1], O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+			write_path_on_fd(fd);
+			ft_putstr_fd("\n", fd);
+			if  (fd != 1)
+				close(fd);
+		}
 	}
-	if (vet_len > 3)
-	{
-		printf("pwd: too many arguments\n");
-		return ;
-	}
-	if (!isset_in_mat(str, "<<"))
-	{
-		if (getcwd(path, sizeof(path)) != NULL)
-			ft_putstr_fd(path, fd);
-		else
-			perror("Erro ao obter o diretório");
-	}
+	else
+		write_path_on_fd(fd);
 	ft_putstr_fd("\n", fd);
-	if (fd != 1)
-		close(fd);
+	if (vet_len > 3 && 0)
+		printf("pwd: too many arguments\n");
 }
--- minishell/src/ft_aux_str.c
+++ minishell/src/ft_aux_str.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   funts_aux.c                                        :+:      :+:    :+:   */
+/*   ft_aux_str.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmateque <hmateque@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:56:22 by hmateque          #+#    #+#             */
-/*   Updated: 2024/10/30 10:31:50 by hmateque         ###   ########.fr       */
+/*   Updated: 2024/10/31 08:15:44 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	isset_in_mat(char **mat, char *str)
 	i = -1;
 	while (mat[++i])
 		if (!ft_strcmp(mat[i], str))
-			return (1);
+			return (i);
 	return (0);
 }
 
@@ -42,4 +42,53 @@ int	ft_strcmp(char *s1, char *s2)
 	return ((int)((unsigned char)s1[i] - (unsigned char)s2[i]));
 }
 
+char *remove_quote(char *str) {
+    int i = 0, j = 0, len = 0;
+    char *new_str;
 
+    while (str[i]) {
+        if (str[i] != '"' && str[i] != '\'')
+            len++;
+        i++;
+    }
+    new_str = (char *)malloc(sizeof(char) * (len + 1));
+    if (!new_str) return NULL;
+
+    i = 0;
+    while (str[i]) {
+        if (str[i] != '"' && str[i] != '\'') {
+            new_str[j++] = str[i];
+        }
+        i++;
+    }
+    new_str[j] = '\0';
+    return new_str;
+}
+
+char **remove_quotes(char **str) {
+    int i = 0, j, qtd_quotes;
+    char    *new_str;
+
+    while (str[i]) {
+        j = 0;
+        qtd_quotes = 0;
+        while (str[i][j]) {
+            if (str[i][j] == '"') qtd_quotes++;
+            if (str[i][j] == '\'') qtd_quotes++;
+            j++;
+        }
+        if (qtd_quotes % 2 == 0) {
+            new_str = remove_quote(str[i]);
+            if (new_str) {
+                free(str[i]);
+                str[i] = new_str;
+            } else {
+                return NULL;
+            }
+        } else {
+            return NULL;
+        }
+        i++;
+    }
+    return str;
+}
